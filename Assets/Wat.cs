0e42f053a40e3e334cda49aae14f0786fa1028aa
Xorboo <xorboo@gmail.com>
2014-08-15 20:19:01 +0400

    Test stuff
    magic cube-companion

---
+++ Assets/Wat.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(1, 10, 1);
+    }
+}
