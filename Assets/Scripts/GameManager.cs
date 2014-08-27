5fc61008a190347cd1a9e7936ad5cb79fb45b99a
Xorboo <xorboo@gmail.com>
2014-08-27 13:27:24 +0400

    Creating players
    Added manager for spawning players, removed junk. Camera still not fixed

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    [Range(1, 4)]
+    public int nuberOfPlayers;
+    public Transform carPrefab;
+    public Vector3[] playerPositions;
+
+	void Start () {
+        CreatePlayers();
+	}
+
+    void CreatePlayers()
+    {
+        for (int i = 0; i < nuberOfPlayers; i++)
+        {
+            var a = Instantiate(carPrefab, playerPositions[i], Quaternion.identity) as Transform;
+            var controller = a.GetComponent<BubbleController>();
+            controller.suffix = "P" + (i + 1).ToString() + "_";
+        }
+    }
+}
