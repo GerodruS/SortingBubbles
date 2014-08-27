2fa5398425469f1f6ace0ad1217ba8ad144eba3e
GerodruS <gerodrus@gmail.com>
2014-08-27 16:33:52 +0400

    Add cooldown to Crash

--- Assets/Crash.cs
+++ Assets/Crash.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Crash : MonoBehaviour
 {
-    public float strength = 1.0f;
+    public float additionalStrength = 1.0f;
+    public float cooldown = 1.0f;
 
     private CarScript car;
+    private float cooldownCurrent = 0.0f;
 
 
     // Use this for initialization
@@ -17,14 +19,26 @@ public class Crash : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (0.0f < cooldownCurrent)
+        {
+            cooldownCurrent -= Time.deltaTime;
+        }
+    }
+
+    void generateBubbles(float value)
+    {
+        Debug.Log("BOOM " + value);
+        int count = 1 + (int)(Random.value * value * 2.0f);
 
     }
 
     void OnCollisionEnter2D(Collision2D coll)
     {
-        if (strength * car.Strength < coll.relativeVelocity.magnitude)
+        if (cooldownCurrent <= 0.0f && additionalStrength * car.Strength < coll.relativeVelocity.magnitude)
         {
-            Debug.Log("BOOM");
+            float value = coll.relativeVelocity.magnitude - additionalStrength * car.Strength;
+            generateBubbles(value);
+            cooldownCurrent = cooldown;
         }
         else
         {
