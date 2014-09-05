86ad1830bcfd34554908cf4fda2b343285203ff4
GerodruS <gerodrus@gmail.com>
2014-09-05 22:34:34 +0400

    Change BubbleController & CameraFollowing interaction with Jets

--- Assets/Scripts/CameraFollowing.cs
+++ Assets/Scripts/CameraFollowing.cs
@@ -4,21 +4,43 @@ using System.Collections;
 public class CameraFollowing : MonoBehaviour
 {
     private Car _car;
-    
+    private BubbleController _bubbleController;
+    private Camera _camera;
+    private float maxAngle = 1.0f;
 
     private void Start()
     {
         _car = GetComponent<Car>();
+        _bubbleController = GetComponent<BubbleController>();
+        _camera = GetComponentInChildren<Camera>();
     }
 
 
     private void FixedUpdate()
     {
-        Vector3 directionCurrent = transform.rotation * Vector3.up;
+        Vector3 directionCurrent = _camera.transform.rotation * Vector3.up;
         Vector2 velocity = _car.rigidbody2D.velocity;
+        velocity.Normalize();
+
+        velocity += _bubbleController.CurrentDirection * 10.0f;
+
         Vector3 directionTarget = new Vector3(velocity.x, velocity.y, 0);
         Quaternion q = Quaternion.FromToRotation(directionCurrent, directionTarget);
-        transform.Rotate(0.0f, 0.0f, q.eulerAngles.z);
+
+        float z = q.eulerAngles.z;
+        if (maxAngle < z && z < 360.0f - maxAngle)
+        {
+            if (180.0f < z)
+            {
+                z = 360.0f - maxAngle;
+            }
+            else
+            {
+                z = maxAngle;
+            }
+        }
+
+        _camera.transform.Rotate(0.0f, 0.0f, z);
     }
 
 }
