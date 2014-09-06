164f95aaedb005616bcfd3e6b154b1e066207202
GerodruS <gerodrus@gmail.com>
2014-09-06 14:43:51 +0400

    Add random walls

---
+++ Assets/RandomWall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomWall : MonoBehaviour
+{
+    public GameObject wall;
+    public int positionsCount = 3;
+
+    private void Start()
+    {
+        Reset();
+    }
+
+    public void Reset()
+    {
+        float start = -0.5f + positionsCount / 2.0f;
+        int n = (int)(Random.value * positionsCount);
+        wall.transform.localPosition = new Vector3(-start + n, 0.0f, 0.0f);
+    }
+}
